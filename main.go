7b2d8a09d4fb439d403ca164a5751e757fd40f39
Circle Yu <circle.yu@nextlink.com.tw>
2020-01-24 14:54:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -10,28 +11,28 @@ import (
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
+	// _, err = bot.SetWebhook(tgbotapi.NewWebhook("https://www.google.com:8443/" + bot.Token))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// info, err := bot.GetWebhookInfo()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if info.LastErrorDate != 0 {
+	// 	log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	// }
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 
 	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
-		}
+		log.Printf("%+v\n", update)
 	}
 }
